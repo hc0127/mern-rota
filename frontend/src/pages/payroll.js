02afd16fcf7c76d0d8b08cf5833012003dcd4caf
heraclessiiz <heracles.siiz@gmail.com>
2022-09-27 21:32:21 +0200

    fix rota

--- frontend/src/pages/payroll.js
+++ frontend/src/pages/payroll.js
@@ -168,10 +168,10 @@ class PayRoll extends Component {
           offDaysPerMonth[selMonth] = [...new Set(offDaysPerMonth[selMonth])];
           dutyHoursPerMonth[selMonth] = (daysInMonth-offDaysPerMonth[selMonth].length)*8;
           if(rotaPerMonth[selMonth] == undefined){rotaPerMonth[selMonth] = 0;}
-          if(dutyHoursPerMonth[selMonth] >= rotaPerMonth[selMonth] && rotaPerMonth[selMonth] >= 192){
+          if(dutyHoursPerMonth[selMonth] >= rotaPerMonth[selMonth]){
             payrollPerMonth[selMonth] = salary;
             payrollCommentPerMonth[selMonth] = comment;
-          }else{
+          }else if(rotaPerMonth[selMonth] >= 192){
             let overtime = rotaPerMonth[selMonth] - dutyHoursPerMonth[selMonth];
             let holidayovertime = 0;
             if(rotaHolidayPerMonth[selMonth] != undefined){
