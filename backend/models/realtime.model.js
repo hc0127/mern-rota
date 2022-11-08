f98bdba3b306fe55684c88d0b161132acb9ebc14
heraclessiiz <heracles.siiz@gmail.com>
2022-11-08 20:43:48 +0100

    again build

--- backend/models/realtime.model.js
+++ backend/models/realtime.model.js
@@ -5,11 +5,8 @@ let Realtime = new Schema({
   request: {
     type: String,//request url
   },
-  level: {
-    type: Number,//1:regitration_add-remove,2:registration_update,3:leave_add-remove,4:leave-update,5:roaster_edit
-  }, 
   status: {
-    type: Number,//1:request,2:approve,3:reject,4:admin edit
+    type: Number,//1:request,2:approve,3:reject
   }, 
   data: {
     type: Object,//request data
